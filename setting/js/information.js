c3066c2f009e3db103bb7879cd1e49672425c458
johnson18205 <johnson18205@gmail.com>
2014-12-16 13:28:49 +0800

    修改檔案位置與內容

---
+++ setting/js/information.js
@@ -0,0 +1,18 @@
+
+
+
+
+        
+    function home1(){
+     alertify.alert('test1');    
+    }
+        
+    function home2(){
+     alertify.alert('test2');
+    }
+    function home3(){
+     alertify.alert('test3');
+    } 
+    function home4(){
+     alertify.alert('test4');
+    }
